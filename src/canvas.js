82b93b08d7afd444b9b76120c10bb54112a16b21
Zeph Requilme <zephrequilme40@gmail.com>
2025-11-09 10:51:43 +0800

    Added a flashlight, changed the particle color, and changed the palette

---
+++ src/canvas.js
@@ -0,0 +1,164 @@
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+
+const scale = 0.5;
+
+const minSpeed = 0.3;
+const maxSpeed = 0.5;
+const minOpacity = 0.1;
+const maxOpacity = 0.2;
+const minAttractedOpacity = 0.8;
+const maxAttractedOpacity = 1;
+
+canvas.height = window.innerHeight * scale;
+canvas.width = window.innerWidth * scale;
+
+ctx.imageSmoothingEnabled = false;
+
+let balls = [];
+
+let mousePos = {
+    x: false,
+    y: false
+}
+
+function randomRange(min, max) {
+    return Math.random() * (max - min) + min;
+}
+
+function randomInt(min, max) {
+    return Math.floor(randomRange(min, max));
+}
+
+function drawBall(ball) {
+    ctx.save();
+
+    ctx.shadowColor = "#fb8b24";
+    ctx.shadowBlur = 8;
+    ctx.fillStyle = "white";
+    ctx.strokeStyle = "#fb8b24";
+    ctx.globalAlpha = ball.opacity;
+    ctx.beginPath();
+    ctx.arc(ball.x, ball.y, ball.size, 0, Math.PI * 2);
+    ctx.fill();
+    ctx.stroke();
+    ctx.closePath();
+
+    ctx.restore();
+}
+
+function newBall() {
+    let ball = {
+        x: randomRange(0, canvas.width),
+        y: randomRange(0, canvas.height),
+        speed: randomRange(minSpeed, maxSpeed) * scale,
+        opacity: 0,
+        maxOpacityOrig: randomRange(minAttractedOpacity, maxAttractedOpacity),
+        maxOpacity: null,
+        direction: randomRange(0, Math.PI * 2),
+        size: 10 * scale,
+        attracted: false,
+        threshold: 200 * scale,
+        dying: false
+    }
+    
+    ball.maxOpacity = ball.maxOpacityOrig;
+    balls.push(ball);
+}
+
+function repositionBalls(oldCanvas, canvas) {
+    balls.forEach((ball, index) => {
+        if(oldCanvas.width != canvas.width) {
+            ball.x *= canvas.width / oldCanvas.width;
+        }
+        if(oldCanvas.height != canvas.height) {
+            ball.y *= canvas.height / oldCanvas.height;
+        }
+    })
+}
+
+for(let i = 0; i < 10; i++) {
+    newBall();
+}
+
+function getDistance(a, b) {
+    return Math.sqrt((a.x - b.x) ** 2 + (a.y - b.y) ** 2);
+}
+
+function getAngle(ball) {
+    return Math.atan2(mousePos.y - ball.y, mousePos.x - ball.x);
+}
+
+function moveBall(ball) {
+    return {
+        x: ball.x + ball.speed * Math.cos(ball.direction),
+        y: ball.y + ball.speed * Math.sin(ball.direction)
+    }
+}
+
+function animate() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    
+    balls.forEach((ball, index) => {
+        if(mousePos.x && getDistance(ball, mousePos) < ball.threshold) {
+            ball.direction = getAngle(ball, mousePos);
+            if(!ball.attracted) {
+                ball.attracted = true;
+            }
+        } else {
+            if(ball.attracted) {
+                ball.attracted = false;
+                ball.direction = randomRange(0, Math.PI * 2);
+            }
+        }
+        
+        if(ball.opacity < ball.maxOpacity) {
+            ball.opacity = Math.min(ball.opacity + 0.075, ball.maxOpacity);
+        }
+        if(ball.opacity > ball.maxOpacity) {
+            ball.opacity = Math.max(ball.opacity - 0.075, ball.maxOpacity);
+        }
+
+        let newPos = moveBall(ball);
+
+        ball.x = newPos.x;
+        ball.y = newPos.y;
+        
+        if((ball.x - ball.size > canvas.width || ball.x - ball.size < 0 || ball.y - ball.size > canvas.height || ball.y - ball.size < 0) && ball.dying == false) {
+            balls.splice(index, 1);
+            newBall();
+        }
+
+        drawBall(ball);
+    })
+
+    requestAnimationFrame(animate);
+}
+
+animate();
+
+document.body.addEventListener("mousemove", (event) => {
+    const canvasRect = canvas.getBoundingClientRect();
+    mousePos.x = (event.clientX - canvasRect.left) * scale;
+    mousePos.y = (event.clientY - canvasRect.top) * scale;
+})
+
+document.body.addEventListener("mouseenter", (event) => {
+    const canvasRect = canvas.getBoundingClientRect();
+    mousePos.x = (event.clientX - canvasRect.left) * scale;
+    mousePos.y = (event.clientY - canvasRect.top) * scale;
+})
+
+document.body.addEventListener("mouseleave", () => {
+    mousePos.x = false;
+})
+
+window.addEventListener("resize", () => {
+    let oldCanvas = {
+        height: canvas.height,
+        width: canvas.width
+    }
+    canvas.height = window.innerHeight * scale;
+    canvas.width = window.innerWidth * scale;
+    repositionBalls(oldCanvas, canvas)
+})
---
+++ src/main.js
@@ -0,0 +1,190 @@
+import "./style.scss";
+import { gsap, ScrollTrigger, ScrollSmoother, ScrollToPlugin } from "gsap/all";
+import { replace } from "feather-icons";
+
+replace();
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother, ScrollToPlugin);
+
+const loadingScreen = document.querySelector("#loading");
+
+const flashlight = document.querySelector("#flashlight");
+
+const heroSection = document.querySelector("#hero");
+const heroScrolldownBtn = document.querySelector("#hero-scrolldown");
+
+const aboutmeSection = document.querySelector("#aboutme");
+
+const projectsLabelSection = document.querySelector("#projects-label");
+const projectsLabel = projectsLabelSection.querySelector("h2");
+const projectsLabelMask = projectsLabelSection.querySelector("#projects-label-mask");
+
+const projectsSection = document.querySelector("#projects");
+const projectsCards = document.querySelector("#projects-cards");
+const projectsCardsEnd = document.querySelector("#projects-cards-end"); 
+
+const sections = [heroSection, aboutmeSection, projectsLabelSection, projectsSection];
+
+const heroScrolldownSpin = gsap.timeline()
+.to(heroScrolldownBtn, { rotation: 360, duration: 0.4, ease: "sine.inOut" })
+.set(heroScrolldownBtn, { rotation: 0 })
+.pause();
+
+function loadAnimations() {
+    gsap.fromTo(heroScrolldownBtn,
+        {
+            opacity: 1
+        },
+        {
+            scrollTrigger: {
+                start: "top bottom",
+                trigger: aboutmeSection,
+                endTrigger: aboutmeSection,
+                end: "top center",
+                scrub: true
+            },
+            opacity: 0
+        }
+    )
+
+    gsap.to(heroSection, {
+        scrollTrigger: {
+            start: "bottom bottom",
+            trigger: heroSection,
+            endTrigger: heroSection,
+            end: "bottom top",
+            pin: heroSection,
+            scrub: true
+        }
+    })
+
+    gsap.fromTo(heroSection,
+        {
+            filter: "blur(0px)",
+        },
+        {
+            scrollTrigger: {
+                trigger: aboutmeSection,
+                start: "top 25%",
+                endTrigger: aboutmeSection,
+                end: "top 10%",
+                scrub: true
+            },
+            filter: "blur(3px)"
+        }
+    )
+
+    gsap.to(projectsLabelSection, {
+        scrollTrigger: {
+            trigger: projectsLabel,
+            start: "center center",
+            endTrigger: projectsCards,
+            end: "center center",
+            scrub: true,
+            pin: projectsLabelSection,
+        }
+    })
+
+    gsap.timeline({
+        scrollTrigger: {
+            trigger: projectsCards,
+            start: "top bottom",
+            endTrigger: projectsCards,
+            end: "top center",
+            scrub: true
+        }
+    })
+    .fromTo(projectsLabel,
+        {
+            opacity: 1
+        },
+        {
+            opacity: 0
+        }
+    )
+
+    gsap.timeline({
+        scrollTrigger: {
+            trigger: projectsLabel,
+            start: "center center",
+            scrub: true,
+            endTrigger: projectsCards,
+            end: "-30% bottom"
+        }
+    })
+    .fromTo(projectsLabel,
+        {
+            opacity: 0,
+        },
+        {
+            opacity: 1,
+            duration: 0.2
+        },
+    )
+    .fromTo(projectsLabel,
+        {
+            filter: "blur(3px)"
+        },
+        {
+            filter: "blur(0px)",
+            duration: 0.1
+        },
+    0.3)
+    
+    gsap.to(projectsCards, {
+        scrollTrigger: {
+            scrub: true,
+            trigger: projectsCards,
+            start: "center center",
+            endTrigger: projectsCardsEnd,
+            end: "top bottom",
+            pin: projectsSection
+        },
+        ease: "sine.inOut",
+        x: -(projectsCards.clientWidth - window.innerWidth)
+    })
+}
+
+function updateProjectsCards() {
+    projectsCardsEnd.style.marginTop = (projectsCards.clientWidth / 2) + "px";
+}
+
+function main() {
+    loadAnimations();
+    updateProjectsCards();
+}
+
+document.addEventListener("mousemove", (ev) => {
+    let flashlightRect = flashlight.getBoundingClientRect();
+
+    gsap.to(flashlight, {
+        x: ev.clientX - (flashlightRect.width / 2),
+        y: ev.clientY - (flashlightRect.width / 2)
+    })
+})
+
+heroScrolldownBtn.addEventListener("click", () => {
+    heroScrolldownSpin.restart();
+    gsap.to(window, {
+        delay: 0.1,
+        scrollTo: aboutmeSection
+    })
+})
+
+document.addEventListener("keypress", (ev) => {
+    if(ev.key == "e") {
+        document.querySelector("#border").innerHTML = document.querySelector("#border").innerHTML ? "" : "* { border: thin solid white }"
+    }
+})
+
+window.scrollTo(0, 0);
+
+window.addEventListener("resize", updateProjectsCards);
+window.addEventListener("load", async () => {
+    await gsap.to(loadingScreen, {
+        opacity: 0,
+        duration: 0.25
+    })
+    document.body.style.overflowY = "auto";
+    main();
+    ScrollTrigger.refresh();
+})
